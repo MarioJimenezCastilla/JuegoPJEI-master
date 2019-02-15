95431db82f974d498f1b328273234d84fa4a0c94
MarioJimenezCastilla <45268376+MarioJimenezCastilla@users.noreply.github.com>
2019-02-15 18:12:56 +0100

    Revisión
    Revisión general del funcionamiento.

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameControl : MonoBehaviour {
+
+    public bool pausa = false;
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown (KeyCode.Escape)) {
+            pausa = !pausa;
+        }
+
+        if (pausa) {
+            Time.timeScale = 0f;
+        } else {
+            Time.timeScale = 1f;
+        }
+    }
+}
